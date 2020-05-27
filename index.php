3d77229b3e704eb4da59523b954a26ea1d4e0efe
WeronikaRo <49307070+WeronikaRo@users.noreply.github.com>
2020-05-27 18:16:40 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
         echo("<input type='submit' value='dodaj'>");
     echo("</form>");
 
-    $results3 = $conn -> query('SELECT id_book, imie, nazwisko, tytul FROM autorzy, book, tytuly WHERE book.id_autor=autorzy.id_autor and book.id_tytul=tytuly.id_tytul');
+    $result3 = $conn -> query('SELECT id_book, imie, nazwisko, tytul FROM autorzy, book, tytuly WHERE book.id_autor=autorzy.id_autor and book.id_tytul=tytuly.id_tytul');
 
     echo("<table class='moja_tabelka'>");
     echo("<tr>
@@ -52,7 +52,7 @@
     <th>tytuł</th>
     </tr>");
 
-    while($row = $results3 -> fetch_assoc()){
+    while($row = $result3 -> fetch_assoc()){
         echo("<tr>");
         echo("<td>".$row['id_book']."</td>");
             echo("<td>".$row['imie']."</td>");
@@ -68,6 +68,59 @@
     };
 
     echo("</table>");
+
+echo("<div class='tab'>");
+echo("<h1>Tabela autorzy:</h1>");
+echo("<h1>Tabela tytuły:</h1>");
+
+    $result4 = $conn -> query("SELECT * FROM autorzy");
+    echo("<div class='a'>");
+    echo("<table class='moja_tabelka'>");
+    echo("<tr>
+    <th>imie</th>
+    <th>nazwisko</th>
+    </tr>");
+
+    while($row = $result4 -> fetch_assoc()){
+        echo("<tr>");
+            echo("<td>".$row['imie']."</td>");
+            echo("<td>".$row['nazwisko']."</td>");
+            echo("<td>
+        <form action='delete.php' method='POST'>
+        <input style='display: none' name='id_to_delete' value=".$row['id_autor'].">
+        <input type='submit' value='delete'>
+        </form>
+        </td>");
+        echo("</tr>");
+    };
+
+    echo("</table>");
+    echo("</div>");
+
+
+    $result4 = $conn -> query("SELECT * FROM tytuly");
+    
+    echo("<div class='a'>");
+    echo("<table class='moja_tabelka'>");
+    echo("<tr>
+    <th>tytu</th>
+    </tr>");
+
+    while($row = $result4 -> fetch_assoc()){
+        echo("<tr>");
+            echo("<td>".$row['tytul']."</td>");
+            echo("<td>
+        <form action='delete.php' method='POST'>
+        <input style='display: none' name='id_to_delete' value=".$row['id_tytul'].">
+        <input type='submit' value='delete'>
+        </form>
+        </td>");
+        echo("</tr>");
+    };
+
+    echo("</table>");
+    echo("</div>");
+    echo("</div>");
 ?>
 </body>
 </html>
